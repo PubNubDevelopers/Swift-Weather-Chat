8228bb8d2c916589779e013921abcebf7dfef7b7
Chandler Mayo <chandler@pubnub.com>
2022-02-14 10:39:59 -0500

    Placeholder text for api keys

--- Sun/Supporting Files/Constants.swift
+++ Sun/Supporting Files/Constants.swift
@@ -7,11 +7,11 @@ import Foundation
 
 struct Constants {
   struct Weather {
-    static let apiKey = "" // Get from openweathermap.org
+    static let apiKey = "your_weather_api_key" // Get from openweathermap.org
   }
 
   struct Pubnub {
-    static let publishKey = "pub-c-key" // Get from Pubnub.com
-    static let subscribeKey = "sub-c-key"
+    static let publishKey = "your_pub_key" // Get from PubNub.com
+    static let subscribeKey = "your_sub_key"
   }
 }
